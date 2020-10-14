23f1e26dc607d9efa7372abff876dbf14a4a287f
GGAGIT <gabriel.gois.andrade14@gmail.com>
2020-10-14 17:30:50 -0300

    alteração no banco de dados.

--- user_api/src/main/kotlin/com/ladys/space/api/constants/ApiConstants.kt
+++ user_api/src/main/kotlin/com/ladys/space/api/constants/ApiConstants.kt
@@ -3,7 +3,7 @@ package com.ladys.space.api.constants
 class ApiConstants private constructor() {
     object CrossOriginConfig {
         const val TEST: String = "http://localhost:8081/occurrences-api"
-        const val DEV: String = "https://occurrences-hdiyc.herokuapp.com/occurrences-api"
+        const val DEV: String = "https://ladys-space-occurrences-api.herokuapp.com/occurrences-api"
     }
 
     object Headers {

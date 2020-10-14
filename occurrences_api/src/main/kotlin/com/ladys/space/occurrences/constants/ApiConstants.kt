64a6a26b0a178ccf69237c3b5151af90669e81d2
GGAGIT <gabriel.gois.andrade14@gmail.com>
2020-10-14 18:31:31 -0300

    alteração na url

--- occurrences_api/src/main/kotlin/com/ladys/space/occurrences/constants/ApiConstants.kt
+++ occurrences_api/src/main/kotlin/com/ladys/space/occurrences/constants/ApiConstants.kt
@@ -2,6 +2,6 @@ package com.ladys.space.occurrences.constants
 
 class ApiConstants private constructor() {
     object Url {
-        const val USER_API: String = "https://ladys-space-user-api.herokuapp.com/api/validate-token"
+        const val USER_API: String = "https://ladys-space-user-api.herokuapp.com/api/validate/token"
     }
 }
